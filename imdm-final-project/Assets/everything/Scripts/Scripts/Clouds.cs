875e2ad772c985ec43169a7fb788c32a92e2404d
Xinyu Yang <carriey170720@gmail.com>
2024-10-27 20:53:06 -0400

    2
    2

---
+++ imdm-final-project/Assets/everything/Scripts/Scripts/Clouds.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Clouds : MonoBehaviour
+{
+    [Header("Effects")]
+    public GameObject particleEffectPrefab;
+
+    [Header("Motion Settings")]
+    public float bobbingAmount = 0.01f;
+    public float bobbingSpeed = 0.01f;    
+
+    private Vector3 startPosition;
+    private float timer;
+
+    void Start()
+    {
+        startPosition = transform.position;
+        timer = Random.Range(0f, Mathf.PI * 2);
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime * bobbingSpeed;
+        float newY = startPosition.y + Mathf.Sin(timer) * bobbingAmount;
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+    }
+}
